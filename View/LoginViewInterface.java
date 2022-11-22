e130c3aaf30c1bd86bd934b66a8300283c455408
Nagulantk <114421488+Nagulantk@users.noreply.github.com>
2022-11-22 11:49:00 +0530

    Delete View directory

--- View/LoginViewInterface.java
+++
@@ -1,9 +0,0 @@
-package View;
-
-import Model.User;
-
-public interface LoginViewInterface {
-     void loginStatus(User user);
-     String userNameInputting();
-     String passWordInputting();
-}
